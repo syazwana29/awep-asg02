a9d367eb2957b4c0c092a3b3612a83abc09b2c32
Syazwana <22FTT1515@student.pb.edu.bn>
2024-04-18 21:07:21 +0800

    added username and password input

--- login.php
+++ login.php
@@ -14,11 +14,19 @@
 <body class="bg-blue-100">
 
   <div>
-    <img src="aweplogo.png">
-    <h1 class="text-purple-900 ml-20 text-4xl font-bold">Welcome</h1>
-    <p class="text-purple-900 ml-20 text-2xl">Log In to contine</p>
+    <img src="aweplogo.png" class="w-32 h-32 mb-0 mt-16">
+    <h1 class="text-purple-900 ml-8 text-2xl font-bold">Welcome</h1>
+    <p class="text-purple-900 ml-8 text-xl">Log In to contine</p>
   </div>
 
+  <div class="mt-36 ml-10">
+    <p class="text-sm font-medium mb-1">Username</p>
+    <input type="email"></input>
+
+    <p class="text-sm font-medium mt-2 mb-1">Password</p>
+    <input type="password"></input>
+
+  </div>
 </body>
 
 </html>
