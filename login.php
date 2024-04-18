2b6e4c0647242c33b3b357f7169261f0b4f542aa
Syazwana <22FTT1515@student.pb.edu.bn>
2024-04-18 20:25:20 +0800

    added tailwind css and background color

--- login.php
+++ login.php
@@ -7,10 +7,11 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <script src="https://cdn.tailwindcss.com"></script>
   <title>Log in Page</title>
 </head>
 
-<body>
+<body class="bg-blue-100">
 
   <img src="aweplogo.png">
 
