43ecb604d9f512ccbd917e087c9b38025bba453f
Syazwana <22FTT1515@student.pb.edu.bn>
2024-04-18 21:33:57 +0800

    added tailwindcss

---
+++ notes.php
@@ -0,0 +1,18 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <script src="https://cdn.tailwindcss.com"></script>
+  <title>Notes</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
