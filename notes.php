0443ad7000531f87bc0913eda43d6adeb3c930a7
Syazwana <22FTT1515@student.pb.edu.bn>
2024-04-22 18:56:11 +0800

    added bg color and 'card' 1

--- notes.php
+++ notes.php
@@ -11,13 +11,18 @@
   <title>Notes</title>
 </head>
 
-<body>
+<body class="bg-blue-100">
   <div>
     <button class="mt-6 ml-4 text-5xl">&lt;</button>
     <h1 class="text-2xl absolute font-semibold ml-36">Notes</h1>
   </div>
 
   <h1 class="font-bold text-3xl ml-6 mt-16">All notes</h1>
+
+  <div class="border rounded-lg px-4 py-4">
+    <p class="text-gray-600">Big Data Analytics...</p>
+    <p class="text-gray-600">For different stages of business analytics...</p>
+  </div>
 </body>
 
 </html>
