16b9b2a0303e5300e6093e3ffd4d2341c0e9eb83
Syazwana <22FTT1515@student.pb.edu.bn>
2024-04-22 19:08:05 +0800

    added 4 'cards'

--- notes.php
+++ notes.php
@@ -19,9 +19,31 @@
 
   <h1 class="font-bold text-3xl ml-6 mt-16">All notes</h1>
 
-  <div class="border rounded-lg px-4 py-4">
-    <p class="text-gray-600">Big Data Analytics...</p>
-    <p class="text-gray-600">For different stages of business analytics...</p>
+  <div class="container mx-auto p-4 mt-4">
+    <div class="bg-white rounded-lg shadow-md p-6">
+      <h1 class="font-bold text-xl">Big Data Analytic</h1>
+      <p class="mt-2">For different stages of business analytics huge amount
+        of data is processed ay various steps. Depending on the
+        stage of...</p>
+    </div>
+    <div class="bg-white rounded-lg shadow-md p-6 mt-2">
+      <h1 class="font-bold text-xl">Data Growth</h1>
+      <p class="mt-2">One of the most pressing Big Data challenges is
+        storage. Data is growing exponentially with time, and
+        with...</p>
+    </div>
+    <div class="bg-white rounded-lg shadow-md p-6 mt-2">
+      <h1 class="font-bold text-xl">Data Challenges</h1>
+      <p class="mt-2">Big Data has seen normalcy in most business today,
+        but that doesn't mean that the journey is always
+        smooth. Ga...</p>
+    </div>
+    <div class="bg-white rounded-lg shadow-md p-6 mt-2">
+      <h1 class="font-bold text-xl">Real Life use cases</h1>
+      <p class="mt-2">Big Data can help you address a range of business
+        activities, from customer experience to analytics. Here
+        are just a few...</p>
+    </div>
   </div>
 </body>
 
