33b0d6e8835193bb71aee29f7fa77df8091198e0
Syazwana <22FTT1515@student.pb.edu.bn>
2024-04-22 19:17:37 +0800

    added background color

--- dataAnalytic.php
+++ dataAnalytic.php
@@ -12,7 +12,11 @@
   <title>Big Data Analytic</title>
 </head>
 
-<body>
+<body class="bg-purple-200">
+  <div>
+    <button class="mt-6 ml-4 text-5xl">&lt;</button>
+    <h1 class="text-2xl absolute font-bold ml-36">Notes</h1>
+  </div>
 
 </body>
 
