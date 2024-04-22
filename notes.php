d08840e45fda86b74040dec8d3c29287ce38681b
Syazwana <22FTT1515@student.pb.edu.bn>
2024-04-22 18:44:08 +0800

    added header 2 "All Notes"

--- notes.php
+++ notes.php
@@ -14,8 +14,10 @@
 <body>
   <div>
     <button class="mt-6 ml-4 text-5xl">&lt;</button>
-    <h1 class="text-2xl absolute font-semibold ml-44">Notes</h1>
+    <h1 class="text-2xl absolute font-semibold ml-36">Notes</h1>
   </div>
+
+  <h1 class="font-bold text-3xl ml-6 mt-16">All notes</h1>
 </body>
 
 </html>
