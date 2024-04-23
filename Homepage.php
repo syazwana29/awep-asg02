7287a97d005ac2dd5f76ae66a52c8f0d895c65f8
22FTT1503 <22FTT1503@student.pb.edu.bn>
2024-04-23 08:14:12 +0800

    added tailwind css script in 3 new added php file

--- Homepage.php
+++ Homepage.php
@@ -4,11 +4,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>Home Page</title>
+
 </head>
 
 <body>
-    <div color-red>1</div>
+
 </body>
 
 </html>
