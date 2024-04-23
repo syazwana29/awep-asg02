b2bdda08269b36067065fcde3921a62feb0d2281
ivy <ivy@gmail.com>
2024-04-23 14:56:18 +0800

    added html

---
+++ aboutpage.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Include Tailwind CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+    <title>Log in Page</title>
+</head>
+
+<body class="bg-gradient-to-b from-white to-blue-200 h-screen relative">
+
+
+
+    <!-- Navigation Bar at the Bottom -->
+    <div class="fixed bottom-0 left-0 w-full bg-yellow-300 p-10 z-10">
+        <ul class="flex justify-center">
+
+        </ul>
+    </div>
+</body>
+
+</html>
