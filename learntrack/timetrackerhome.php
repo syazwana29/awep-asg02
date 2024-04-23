fce0f6a8ea3bb8b346f124c4195ab0fb53175a98
22FTT1518 <22ftt1518@student.pb.edu.bn>
2024-04-23 12:28:17 +0800

    add circular icon on nav bar

--- learntrack/timetrackerhome.php
+++ learntrack/timetrackerhome.php
@@ -11,14 +11,12 @@
 
 <body class="bg-gradient-to-b from-white to-blue-200 h-screen relative">
 
-
-
     <!-- Navigation Bar at the Bottom -->
-    <div class="fixed bottom-0 left-0 w-full bg-yellow-300 p-10 z-10">
-        <ul class="flex justify-center">
-
-        </ul>
+    <div class="fixed bottom-0 left-0 w-full bg-yellow-300 p-10 z-10 flex justify-center">
+        <!-- Circular element protruding out with thicker border -->
+        <div class="bg-white h-20 w-20 rounded-full absolute bottom-0 transform -translate-y-1/2 translate-y-1/3 border-8 border-yellow-300"></div>
     </div>
+
 </body>
 
 </html>
