5da8e23831626b410ce201a009e94c0200e10a61
22FTT1519 <22ftt1519@student.pb.edu.bn>
2024-05-07 13:15:27 +0800

    test html

--- studyschedule.php
+++ studyschedule.php
@@ -1,4 +1,44 @@
 <!DOCTYPE html>
+<html>
+
+<head>
+    <title>Study Schedule</title>
+    <link rel="stylesheet" type="text/css" href="studyschedule.css">
+</head>
+
+<body>
+    <div class="header">
+        <a href="#" id="back-button"><img src="assets/left-arrow.png"></a>
+        <h1>Study Schedule</h1>
+        <a href="#" id="calendar-symbol"><img src="assets/Studysched.png"></a>
+    </div>
+    <span class="date-of-today">
+        <div id="datetdy"><?php echo date("d/m"); ?></div>
+        <div>Today</div>
+    </span>
+    <div class="daysinweek">
+        <?php
+        $daysInWeek = array("Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun");
+        foreach ($daysInWeek as $day) {
+            echo "<div class='day' onclick='openBoard(\"$day\")'>$day</div>";
+        }
+        ?>
+    </div>
+    <div id="boards">
+        <?php
+        foreach ($daysInWeek as $day) {
+            echo "<div class='board' id='board-$day'></div>";
+        }
+        ?>
+    </div>
+    <script src="scripts.js">
+    </script>
+</body>
+
+</html>
+
+
+<!DOCTYPE html>
 <html lang="en">
 
 <head>
