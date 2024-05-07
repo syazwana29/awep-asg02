6564497b1b8ff58e9885fc87d15cf4eba9ce675d
22FTT1503 <22FTT1503@student.pb.edu.bn>
2024-05-07 13:42:22 +0800

    chore: Refactor homepage layout and improve accessibility
    takenote: homepage is finished, needed to connect those button with other html

--- Homepage.php
+++ Homepage.php
@@ -9,57 +9,69 @@
     <title>Home Page</title>
 </head>
 
-<body class="bg-gradient-to-b from-white to-blue-200 h-screen relative">
+<body class="bg-gradient-to-b from-white to-blue-200 h-screen relative flex flex-col">
     <div style="display: flex; justify-content: space-between;">
         <img src="./assets/filters.png" alt="filter" class="w-10 h-10">
-        <p class="text-xl font-bold">Home</p>
+
         <img src="./assets/smallLogo.png" alt="logo" class="w-14 h-14">
     </div>
+    <p class="text-xl font-bold text-center">Home</p>
     <br>
     <div>
         <p class="text-2xl font-bold">Welcome</p>
         <p>We're here to assist you to explore educational journey</p>
         <br>
         <br>
+
         <div class="container mx-auto">
-            <div class="flex justify-between">
-                <div class="bg-white rounded-lg p-4"> <img src="./assets/Studysched.png" alt="schedule">
-                    Study Schedule</div>
-                <div class="bg-white rounded-lg p-4"><img src="./assets/timetracker.png" alt="time tracker">Time Tracking</div>
-                <div class="bg-white rounded-lg p-4"><img src="./assets/task.png" alt="task manager">Task Manager</div>
+            <div class="flex justify-around">
+                <div class="bg-white rounded-lg p-4 flex flex-col items-center justify-center ">
+                    <img src="./assets/Studysched.png" alt="schedule">
+                    <p class="text-lg">Study Schedule</p>
+                </div>
+                <div class="bg-white rounded-lg p-6 flex flex-col items-center justify-center">
+                    <img src="./assets/timetracker.png" alt="time tracker" class="h-30 w-30">
+                    <p class="text-lg">Time Tracking</p>
+                </div>
+                <div class="bg-white rounded-lg p-4 flex flex-col items-center justify-center">
+                    <img src="./assets/task.png" alt="task manager">
+
+                    <p class="text-lg">Task Manager</p>
+                </div>
             </div>
         </div>
         <br>
-        <p class="text-xl font-bold">Others</p>
+        <p class="text-xl font-bold"> Others</p>
+        <br>
         <div class="container mx-auto">
-            <div class="flex justify-between">
-                <div class="bg-white rounded-lg p-4">Notes</div>
-                <div class="bg-white rounded-lg p-4">Exam</div>
+            <div class="flex mx-auto justify-center space-x-4">
+                <div class="bg-white rounded-lg p-4 flex items-center justify-center" style="width:210px; font-size:20px;">Notes</div>
+                <div class="bg-white rounded-lg p-4 flex items-center justify-center" style="width:210px; font-size:20px;">Exam</div>
             </div>
         </div>
-        <br><br>
+        <br>
         <div class="container mx-auto">
-            <div class="bg-white rounded-lg p-4">
-                <p>"Aim to be 1% better than yesterday"</p>
-                <p>- Unknown.</p>
+            <div style="height: 110px; width: 450px; font-size:20px;" class="bg-white rounded-lg p-4 flex flex-col items-center justify-center mx-auto">
+                <p>"Aim to be 1% better </p>
+                <p>than yesterday"</p>
+                <p> - Unknown.</p>
             </div>
             <br>
-            <div class="bg-white rounded-lg p-4">
+            <div style="font-size:20px; background-color: #78318C; border: 2px solid white; height: 50px;" class="rounded-lg p-4 w-1/2 mx-auto flex items-center justify-center text-center text-white">
                 About Us
             </div>
         </div>
 
-    </div>
-    </div>
 
 
 
-    <!-- Navigation Bar at the Bottom -->
-    <div class=" fixed bottom-0 left-0 w-full bg-yellow-300 p-10 z-10">
-        <ul class="flex justify-center">
+        <!-- Navigation Bar at the Bottom -->
 
-        </ul>
-    </div>
+        <footer class="fixed bottom-0 left-0 w-full  bg-yellow-300 ">
+            <ul class="flex justify-end">
+                <p class="text-purple-700">LearnTrack.inc</p>
+            </ul>
+        </footer>
 </body>
 
 </html>
