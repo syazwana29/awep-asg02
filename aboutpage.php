c38d9cc78612e3d233e52a5ed40509c297080d6f
ivy <ivy@gmail.com>
2024-05-08 07:59:47 +0800

    added paragraph on aboutpage

--- aboutpage.php
+++ aboutpage.php
@@ -14,6 +14,35 @@
 </head>
 
 <body class="bg-gradient-to-b from-white to-blue-200 h-screen relative">
+    <h1>about page</h1>
+
+    <p>
+        LearnTrack is dedicated to development of high-quality software,
+        with the goal of simplifying the learning process for users.
+        The platform is designed to be user-friendly, enabling users to track
+        their progress and maintain motivation while studying.
+    </p>
+    <br>
+    <p>
+        Furthermore, LearnTrack is focused on making quality education accessible to
+        users regardless of their location or available resources.
+        Valuable insights into study habits are offered to help students devise effective learning strategies.
+        The guiding principle is to prioritize the user experience,
+        ensuring that the app remains accessible to a wide range of users.
+    </p>
+    <br>
+    <p>
+        Regular updates to the app are made based on user feeback
+        and emerging trends in education and technology.
+    </p>
+    <br>
+    <p>
+        The strategic objective of LearnTrack is to achive user growth by
+        gaining more users through marketing campaigns and user referrals.
+        This will achieved by implementing a user-friendly interface,
+        providing regular progress reports, and offering personalized study recommendations.
+        The aim is to increase user engagement by at least 50% among registered users.
+    </p>
 
 
     <!-- Navigation Bar at the Bottom -->
