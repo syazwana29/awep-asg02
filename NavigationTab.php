7bc8f822dceddb9b14a787ddc7a3edcffa08ccf3
22FTT1503 <22FTT1503@student.pb.edu.bn>
2024-05-07 15:50:29 +0800

    chore: Update NavigationTab.php to improve navigation icons layout

--- NavigationTab.php
+++ NavigationTab.php
@@ -10,28 +10,35 @@
 
 <body style="background-color: rgba(170, 81, 196, 0.8);" class="h-screen relative">
     <!-- Your content here -->
-    <div>
-        <img src="./assets/Studysched.png" alt="study">
-        <p>Study Schedule</p>
+    <div style="display: flex; ">
+        <div class="bg-white rounded-lg p-4  ">
+            <img src="./assets/Studysched.png" alt="study">
+            <p>Study Schedule</p>
+        </div>
+        <div class="bg-white rounded-lg p-4  ">
+            <img src="./assets/alarm.png" alt="time">
+            <p>Time Tracking</p>
+        </div>
     </div>
-    <div>
-        <img src="./assets/alarm.png" alt="time">
-        <p>Time Tracking</p>
+    <div style="display: flex; ">
+        <div class="bg-white rounded-lg p-4  ">
+            <img src="./assets/smallLogo.png" alt="home">
+            <p>Home</p>
+        </div>
+        <div class="bg-white rounded-lg p-4  ">
+            <img src="./assets/task.png" alt="task">
+            <p>Task Manager</p>
+        </div>
     </div>
-    <div>
-        <img src="./assets/smallLogo.png" alt="home">
-        <p>Home</p>
-    </div>
-    <div>
-        <img src="./assets/task.png" alt="task">
-        <p>Task Manager</p>
-    </div>
-    <div>
-        <img src="./assets/note.png" alt="note">
-        <p>Notes</p>
-    </div>
-    <div>
-        <p>Exam</p>
+    <div style="display: flex; ">
+        <div class="bg-white rounded-lg p-4  ">
+            <img src="./assets/notes.png" alt="note">
+            <p>Notes</p>
+        </div>
+        <div class="bg-white rounded-lg p-4  ">
+            <img src="./assets/exam.png" alt="exam">
+            <p>Exam</p>
+        </div>
     </div>
 </body>
 
