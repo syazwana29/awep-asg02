4e4cf92e57111d7210e050f592d0f57eb8bbd4c2
Syazwana <22FTT1515@student.pb.edu.bn>
2024-05-09 23:31:24 +0800

    modified typo

--- login.php
+++ login.php
@@ -13,7 +13,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   $login = new mysqli($servername, $db_username, $db_password, $dbname);
 
   if ($login->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+    die("Connection failed: " . $login->connect_error);
   }
 
   $sql = "SELECT * FROM users WHERE username = '$username' AND password = '$password'";
