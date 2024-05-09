6f712d67ff3b6040839fc3b228a9a3aec00963c2
22FTT1503 <22FTT1503@student.pb.edu.bn>
2024-05-09 21:43:07 +0800

    added pic, update setting

--- setting.php
+++ setting.php
@@ -14,4 +14,13 @@
 </body>
 <img src=" ./assets/left-arrow.png" alt="backarrow" class="h-10 w-10" onclick="window.history.back();">
 
+<h1>Jane Doe</h1>
+<p>Jane Doe </p>
+<p>+673 123 4567</p>
+<p>Password</p>
+
+<div style="font-size:30px; background-color: #78318C; border: 3px solid white; height: 50px;" class="rounded-lg p-4 w-1/2 mx-auto flex items-center justify-center text-center text-white" onclick="window.location.href='login.php'">
+    Sign Out
+</div>
+
 </html>
